0aa4063183a4d9d3139778f8c49de663f0d1f8dc
Zoljargal <zol.fallows@hotmail.com>
2019-05-22 01:16:21 +0100

    some test cases are not passing

--- test/clone-object.js
+++ test/clone-object.js
@@ -3,6 +3,11 @@ describe('clone object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
 
+    for(let key in expected){
+      let value = expected[key];
+      obj[key] = value;
+    }
+
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,28 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    let res = []
+
+    const recursiveFunc = (data) => {
+        if(Array.isArray(data)){
+          for(let i=0; i<data.length; i++){
+            if(Array.isArray(data[i])){
+              recursiveFunc(data[i])
+            }else{
+              res.push(data[i])
+            }
+          }
+        } else {
+          res.push(data)
+        }
+    } 
+    
+    for(let i=0; i<arr.length; i++){
+      recursiveFunc(arr[i])
+    }
+
+    arr = res.sort()
+    
     expect(arr).toEqual(expected);
   });
 });
